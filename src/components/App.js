ec7f1ae2d65589e819b1c28d7aca807fab8e54d9
Heather You <heatherhyou@gmail.com>
2018-04-02 21:40:56 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import TweetWall from './TweetWall';
 import { getTweets }from '../lib/mockAPI';
 
 class App extends React.Component {
-
   constructor() {
     super();
 
@@ -14,8 +13,19 @@ class App extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount() {
+    this.fetchTweets()
+  }
+
   // TODO: componentDidMount()
+  componentDidMount() {
+    this.startInterval()
+  }
+
   // TODO: componentWillUnmount()
+  componentWillUnmount() {
+    this.cleanUpInterval()
+  }
 
   startInterval = () => {
     this.interval = setInterval(this.fetchTweets, 2000);
